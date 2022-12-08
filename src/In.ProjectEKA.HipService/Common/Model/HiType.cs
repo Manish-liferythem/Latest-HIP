10bc8de0e8289d660e4fd1559f87d2a6a5474291
SanoferSameera <sanofersameera25@gmail.com>
2022-12-08 17:18:57 +0530

    Refactor. Hi Types enum
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- src/In.ProjectEKA.HipService/Common/Model/HiType.cs
+++ src/In.ProjectEKA.HipService/Common/Model/HiType.cs
@@ -2,17 +2,12 @@ namespace In.ProjectEKA.HipService.Common.Model
 {
     public enum HiType
     {
-        Condition,
-        Observation,
         DiagnosticReport,
-        MedicationRequest,
-        DocumentReference,
         Prescription,
         ImmunizationRecord,
         DischargeSummary,
         OPConsultation,
         WellnessRecord,
         HealthDocumentRecord
-        
     }
 }
